f6d7bb923c8f0a62287b4f8af06b5292b84bc5e0
shantanuk7 <shantanukulkarni365@gmail.com>
2026-02-01 11:50:24 +0530

    chore: add requirement for commit message to have description
    Adds some more lines to commitlint config to ensure commit body is always non-empty

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,3 +1,10 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    // Require a blank line between subject and body
+    'body-leading-blank': [2, 'always'],
+
+    // Require non-empty body
+    'body-empty': [2, 'never'],
+  },
 };
